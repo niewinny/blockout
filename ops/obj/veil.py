472f59fcd9da20a957a9fc24c6215d097cfb515c
niewinny <the.tecc@gmail.com>
2025-08-22 07:29:54 +0200

    feat: veil and unveil to work in local mode

--- ops/obj/veil.py
+++ ops/obj/veil.py
@@ -9,10 +9,19 @@ class BOUT_OT_Veil(bpy.types.Operator):
     bl_options = {'REGISTER', 'UNDO'}
 
     def execute(self, context):
-        for obj in bpy.context.visible_objects:
-            if obj.type == 'MESH' and obj.visible_get() and obj.display_type == 'WIRE':
-                if not obj.select_get():
-                    obj.hide_set(True)
+        # Get the 3D view space
+
+        space = context.space_data 
+        in_local_view = space and hasattr(space, 'local_view') and space.local_view
+        
+        # Hide all non-selected wire mesh objects
+        for obj in context.visible_objects:
+            if obj.type == 'MESH' and obj.display_type == 'WIRE' and not obj.select_get():
+                # If in local view, first remove from local view
+                if in_local_view:
+                    obj.local_view_set(space, False)
+                # Then hide the object
+                obj.hide_set(True)
 
         return {'FINISHED'}
 
@@ -24,6 +33,9 @@ class BOUT_OT_Unveil(bpy.types.Operator):
     bl_options = {'REGISTER', 'UNDO'}
 
     def execute(self, context):
+        # Check if we're in local view (isolated objects)
+        space = context.space_data
+        in_local_view = space and hasattr(space, 'local_view') and space.local_view
 
         selected_objects = context.selected_objects
         boolean_objects = set()
@@ -36,6 +48,9 @@ class BOUT_OT_Unveil(bpy.types.Operator):
 
         for obj in boolean_objects:
             obj.hide_set(False)
+            # If in local view, also add the object to local view
+            if in_local_view and not obj.local_view_get(space):
+                obj.local_view_set(space, True)
 
         return {'FINISHED'}
 
