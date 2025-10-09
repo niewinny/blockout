bb34f2b54902d1eaaa4bd42edab7c5090917aaaa
niewinny <the.tecc@gmail.com>
2025-10-09 20:29:46 +0200

    version: 1.1.1

--- __init__.py
+++ __init__.py
@@ -19,7 +19,7 @@ bl_info = {
     'name': "Blockout",
     'description': "Mesh Modeling Toolkit",
     'author': "ezelar.com",
-    'version': (1, 1, 0),
+    'version': (1, 1, 1),
     'blender': (4, 2, 0),
     'location': 'View3D',
     'wiki_url': '',
