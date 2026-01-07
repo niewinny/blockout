8dec59b9ab2131e28d33d1d3ecf2445d6adb83f6
niewinny <the.tecc@gmail.com>
2026-01-07 17:18:30 +0100

    feat: pref to use __package__

--- utils/addon.py
+++ utils/addon.py
@@ -3,11 +3,11 @@ from pathlib import Path
 import bpy
 import tomllib
 
+from .. import __package__ as _package_name
+
 _manifest_path = Path(__file__).parent.parent / "blender_manifest.toml"
 with _manifest_path.open("rb") as _f:
     _manifest = tomllib.load(_f)
-
-_package_name = __name__.rsplit(".", 2)[0]
 
 version: str = _manifest["version"]
 version_tuple: tuple[int, ...] = tuple(int(x) for x in version.split("."))
