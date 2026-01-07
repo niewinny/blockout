386205d3f8b421c367e768f79e7bb7b8b18c4a74
niewinny <the.tecc@gmail.com>
2026-01-07 17:15:02 +0100

    feat: move version to addon utils

--- __init__.py
+++ __init__.py
@@ -1,7 +1,3 @@
-from pathlib import Path
-
-import tomllib
-
 from . import (
     btypes,
     gizmo,
@@ -12,13 +8,6 @@ from . import (
     ui,
     utils,
 )
-
-_manifest_path = Path(__file__).parent / "blender_manifest.toml"
-with _manifest_path.open("rb") as _f:
-    _manifest = tomllib.load(_f)
-
-__version__ = _manifest["version"]
-__version_tuple__ = tuple(int(x) for x in __version__.split("."))
 
 __all__ = ["btypes", "gizmo", "ops", "preferences", "registry", "tools", "ui", "utils"]
 
